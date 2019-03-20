0c431826c0ff31eb24eb5e4f72fbe8fc37c10be5
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-20 18:01:10 -0400

    Created folders for post and user routes/db

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const server = express();
 
+const userRouter = require("./userRoute/router.js");
+const postRouter = require("./postRoute/router.js");
+
+server.use(express.json());
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
 module.exports = server;
